b121ca9b1af79d295b9450d6f84f6678939a0938
murdoaird <murdo@murdo-Aspire-5733.(none)>
2012-11-13 22:07:48 +0000

    a working app

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module HealthMap
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    #don't log passwords
+    config.filter_parameters += [:password, :password_confirmation]
   end
 end
