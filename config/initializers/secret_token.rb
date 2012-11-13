7265a046907af178bf8085e7f6df76ffdd7ff537
murdoaird <murdo@murdo-Aspire-5733.(none)>
2012-11-13 20:28:51 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HealthMap::Application.config.secret_token = '253dafe1e3d0515af20e23c7cba9a16a51090cc01d0c485451c7e9a923ebded3c87af41cd8b2c5c84f5d5430e9e4d7fb968749382f76688fa9d64600533f4804'
