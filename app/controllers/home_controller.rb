d994d36bc9afce38db81ebdb3f84bdd3141a29ec
murdoaird <murdo@murdo-Aspire-5733.(none)>
2012-11-15 10:53:23 +0000

    added json

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @states_json = Rails.root.join("lib/data/us-states.json").read
     @users = User.all
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,11 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 HealthMap::Application.initialize!
+
+# added by murdo to fix debug error
+class String
+  def is_binary_data?
+    ( self.count( "^ -~", "^\r\n" ).fdiv(self.size) > 0.3 || self.index( "\x00" ) ) unless empty?
+  end
+end
+  # end of murdo code
